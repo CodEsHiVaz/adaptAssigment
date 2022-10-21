e17cdfb1f0b506546f3e34bebdede814b5d1d68d
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2022-10-21 12:44:32 +0530

    done

---
+++ index.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppeteer");
+(async () => {
+  let scrapurl =
+    "https://www.officedepot.com/a/browse/ergonomic-office-chairs/N=5+593065&amp;cbxRefine=1429832/";
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(scrapurl);
+  //   await page.click("#sorter", { delay: 1000 });
+
+  await page.select(
+    "#sorter",
+    "/catalog/search.do;jsessionid=0000HUwrRcBbvO4xXJ_atzbdHHU:17h4h7ceo?seoText=ergonomic-office-chairs&%252525253BcbxRefine=1429832&N=5+593065&recordsPerPageNumber=24&Ns=p_AVERAGE_RATING%7C1"
+  );
+  await page.waitForSelector("#productView");
+
+  await page.waitForSelector("#productView");
+
+  let data = await page.evaluate(() => {
+    let pagName = document.querySelector(
+      ".desc_text>a"
+    ).textContent;
+
+    return {
+      pagName,
+    };
+  });
+    console.log(data);
+  await browser.close();
+})();
